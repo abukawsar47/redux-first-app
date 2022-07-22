a782791d50248f938816bc4ffa4a2b322292bd23
abukawsar47 <abukawsar47ak@gmail.com>
2022-07-23 00:17:46 +0600

    reducer

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const INCREMENT = "INCREMENT";
+const DECREMENT = "DECREMENT";
+// const ADD_USER = "ADD_USER";
 // state
 const initialCounterState = {
   count: 0,
@@ -6,14 +9,53 @@ const initialCounterState = {
 const initialUserState = {
   users: [{ name: "Abu Kawsar" }],
 };
-
+//add user is an action
 //actions => Object => type, payload
 //INCREMENT COUNTER
-const INCREMENT = "INCREMENT";
+
 const incrementCounter = () => {
   return {
     type: INCREMENT,
   };
 };
-
+// Action will have two things type and payload
 //DECREMENT COUNTER
+
+const decrementCounter = () => {
+  return {
+    type: DECREMENT,
+  };
+};
+//add user
+// When the action type is dispatched then the data must also be dispatched
+/* const addUser = () => {
+  return {
+    type: ADD_USER,
+  };
+}; */
+
+//1. state
+//2. dispatch action
+//3. reducer
+//4. store
+
+// Reducer - create reducer for counter
+
+const counterReducer = (state = incrementCounter, action) => {
+  switch (action.type) {
+    case INCREMENT:
+      return {
+        ...state,
+        const: state.count + 1,
+      };
+
+    case DECREMENT:
+      return {
+        ...state,
+        const: state.count - 1,
+      };
+
+    default:
+      state;
+  }
+};
